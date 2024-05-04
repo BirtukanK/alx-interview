be56767716a7fc824b844733a2a0ee064feefa42
BirtukanK <birtukankuma1113@gmail.com>
2024-05-04 11:29:13 +0000

    fix code style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,18 +6,14 @@ def canUnlockAll(boxes):
     n = len(boxes)
     if n == 0:
         return False
-    
     visited = set()
     to_visit = [0]
     visited.add(0)
-    
     while to_visit:
         current_box = to_visit.pop(0)
         keys = boxes[current_box]
-        
         for key in keys:
             if key < n and key not in visited:
                 visited.add(key)
                 to_visit.append(key)
-    
     return len(visited) == n
